371fd6330d2e19fbb4441501b031097ca905953d
Carjooj <victor@4info.com.br>
2024-11-10 19:23:44 -0300

    feat: iniciando projeto Spring da API

---
+++ ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/ChromodoroApiApplication.java
@@ -0,0 +1,13 @@
+package fatec.jvprojects.chromodoroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChromodoroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChromodoroApiApplication.class, args);
+    }
+
+}
