33799cdbc7f9cdfcd044256290d7bb4aa3b59e44
Carjooj <victor@4info.com.br>
2024-11-13 23:22:50 -0300

    feat: Criando interface de serviço para Tarefa, e implementação

--- ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/service/ITarefaServico.java
+++ ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/service/ITarefaServico.java
@@ -1,4 +1,18 @@
 package fatec.jvprojects.chromodoroapi.service;
 
-public interface ITarefaRepository {
+import fatec.jvprojects.chromodoroapi.model.Tarefa;
+import fatec.jvprojects.chromodoroapi.model.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ITarefaServico {
+    Optional<Tarefa> salvarTarefa(Tarefa tarefa);
+
+    List<Tarefa> listarTarefasUsuario(Usuario usuario);
+
+    Optional<Tarefa> atualizarTarefa(Tarefa tarefa);
+
+    void excluirTarefa(Tarefa tarefa);
+
 }
--- ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/service/TarefaServico.java
+++ ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/service/TarefaServico.java
@@ -1,4 +1,49 @@
 package fatec.jvprojects.chromodoroapi.service;
 
-public class TarefaServico {
+import fatec.jvprojects.chromodoroapi.model.Tarefa;
+import fatec.jvprojects.chromodoroapi.model.Usuario;
+import fatec.jvprojects.chromodoroapi.repository.ITarefaRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TarefaServico implements ITarefaServico {
+
+    Logger logger = LogManager.getLogger(this.getClass());
+
+    ITarefaRepository tarefaRepository;
+
+    public TarefaServico(ITarefaRepository tarefaRepository) {
+        this.tarefaRepository = tarefaRepository;
+    }
+
+    @Override
+    public Optional<Tarefa> salvarTarefa(Tarefa tarefa) {
+        logger.info("|--- Serviço - Salvar tarefa ---|");
+
+        return Optional.of(tarefaRepository.insert(tarefa));
+    }
+
+    @Override
+    public List<Tarefa> listarTarefasUsuario(Usuario usuario) {
+        logger.info("|--- Serviço - Listar tarefas do usuário ---|");
+
+        return tarefaRepository.findTarefasByIdUsuario(usuario);
+    }
+
+    @Override
+    public Optional<Tarefa> atualizarTarefa(Tarefa tarefa) {
+        logger.info("|--- Serviço - Atualizar tarefa ---|");
+
+        return Optional.of(tarefaRepository.save(tarefa));
+    }
+
+    @Override
+    public void excluirTarefa(Tarefa tarefa) {
+        logger.info("|--- Serviço - Excluir tarefa ---|");
+
+        tarefaRepository.delete(tarefa);
+    }
 }
