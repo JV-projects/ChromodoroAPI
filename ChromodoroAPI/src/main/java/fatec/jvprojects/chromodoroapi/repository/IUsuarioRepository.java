4f09cbc8be478c98bccd759b7549ace15c3c05b2
Carjooj <victor@4info.com.br>
2024-11-13 23:22:23 -0300

    feat: Adicionando método ao IUsuarioRepository

--- ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/repository/IUsuarioRepository.java
+++ ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/repository/IUsuarioRepository.java
@@ -3,6 +3,8 @@ package fatec.jvprojects.chromodoroapi.repository;
 import fatec.jvprojects.chromodoroapi.model.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface IUsuarioRepository extends MongoRepository<Usuario, String> {
+import java.util.Optional;
 
+public interface IUsuarioRepository extends MongoRepository<Usuario, String> {
+    Optional<Usuario> findUsuarioByEmail(String email);
 }
