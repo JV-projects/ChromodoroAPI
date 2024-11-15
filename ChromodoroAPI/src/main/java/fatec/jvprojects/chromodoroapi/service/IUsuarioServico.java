17c3a7df28e03b2bc17af4ea4e689672c4b8a20e
Carjooj <victor@4info.com.br>
2024-11-15 15:27:58 -0300

    feat: Criando interface e classe de serviço para usuário

--- ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/service/IUsuarioServico.java
+++ ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/service/IUsuarioServico.java
@@ -1,4 +1,18 @@
 package fatec.jvprojects.chromodoroapi.service;
 
+import fatec.jvprojects.chromodoroapi.model.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IUsuarioServico {
+    Optional<Usuario> salvarUsuario(Usuario usuario);
+
+    List<Usuario> listarUsuarios();
+
+    Optional<Usuario> encontrarPorEmail(String email);
+
+    Optional<Usuario> atualizarUsuario(Usuario usuario);
+
+    void deletarUsuario(Usuario usuario);
 }
--- ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/service/UsuarioServico.java
+++ ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/service/UsuarioServico.java
@@ -1,4 +1,57 @@
 package fatec.jvprojects.chromodoroapi.service;
 
-public class UsuarioServico {
+import fatec.jvprojects.chromodoroapi.model.Usuario;
+import fatec.jvprojects.chromodoroapi.repository.IUsuarioRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioServico implements IUsuarioServico {
+
+    Logger logger = LogManager.getLogger(this.getClass());
+
+    private IUsuarioRepository usuarioRepository;
+
+    public UsuarioServico(IUsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+    @Override
+    public Optional<Usuario> salvarUsuario(Usuario usuario) {
+        logger.info("|--- Serviço - Salvar usuário ---|");
+
+        return Optional.of(usuarioRepository.insert(usuario));
+    }
+
+    @Override
+    public List<Usuario> listarUsuarios() {
+        logger.info("|--- Serviço - Listar usuários ---|");
+
+        return usuarioRepository.findAll();
+    }
+
+    @Override
+    public Optional<Usuario> encontrarPorEmail(String email) {
+        logger.info("|--- Serviço - Encontrar usuário por email ---|");
+
+        return usuarioRepository.findUsuarioByEmail(email);
+    }
+
+
+    @Override
+    public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+        logger.info("|--- Serviço - Atualizar usuário ---|");
+
+        return Optional.of(usuarioRepository.save(usuario));
+    }
+
+    @Override
+    public void deletarUsuario(Usuario usuario) {
+        logger.info("|--- Serviço - Deletar usuário ---|");
+    }
 }
