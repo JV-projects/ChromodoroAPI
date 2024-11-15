cf67586cbf66f3ac2d8331161cc89beb677e15c5
Carjooj <victor@4info.com.br>
2024-11-15 19:20:58 -0300

    feat: Criando serviço de autenticação para o Spring Security

--- ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/service/AutorizacaoServico.java
+++ ChromodoroAPI/src/main/java/fatec/jvprojects/chromodoroapi/service/AutorizacaoServico.java
@@ -1,4 +1,22 @@
 package fatec.jvprojects.chromodoroapi.service;
 
-public class AutorizacaoServico {
+import fatec.jvprojects.chromodoroapi.repository.IUsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorizacaoServico implements UserDetailsService {
+
+    private IUsuarioRepository usuarioRepository;
+
+    public AutorizacaoServico(IUsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findUsuarioByEmail(username).get();
+    }
 }
